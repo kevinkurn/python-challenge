24cff8188198bba22022c5b07ac297e78d843c68
kevinkurn <kevin_sebastian@hotmail.com>
2021-06-30 11:51:24 +0800

    finalised code and output for Pybank Exercise

---
+++ Pybank/main.py
@@ -0,0 +1,81 @@
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('..', 'Pybank', 'Resources', 'budget_data.csv')
+output_path= os.path.join('..', 'Pybank', 'Analysis', 'test.txt')
+
+#module to upload output as txt
+import sys
+
+#import statistics module
+import statistics
+
+#Open the csvfile
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #skip the first header of the file
+    csv_header=next(csvreader)
+    #create a counter to count the total number of months (this is done by counting number of rows excl. the header in the file)
+    total_month=0   
+    #create a counter to count total profit/loss with starting value of 0
+    total_profit=0
+    #create a blank list to input all the profit/loss month to month
+    profit_list=[]
+    #create a blank list to calculate changes of profit/loss on month to month
+    var_list = []
+    #create a blank list to store all the months in csv file
+    month_list=[]
+    #create a counter to allow substraction calculation between profits in the list
+    profit_counter = 0
+
+    # Read each row of data after the header
+    for row in csvreader:
+        #increase total month counter by one
+        total_month+=1
+        #define total profit of the month
+        total_profit=total_profit+float(row[1])   
+        #update profit list with profit figure month on month
+        profit_list.append(row[1])
+        #update month_list
+        month_list.append(row[0])
+
+
+    #create a loop that calculates the change profit/loss from current month to the next month
+    for i in range(len(profit_list)-1):
+        diff=(float(profit_list[profit_counter+1]))-float(profit_list[profit_counter])
+        var_list.append(diff)
+        profit_counter=profit_counter+1
+
+    avg_change=float(statistics.mean(var_list))
+    greatest_inc = max(var_list)
+    greatest_dec = min(var_list)
+    highest_month = var_list.index(greatest_inc)+1
+    lowest_month = var_list.index(greatest_dec)+1
+
+    #print output in terminal
+    
+    print("Financial Analysis")
+    print("----------------------")
+    print(f"Total Months: {total_month}")
+    print(f"Total: {total_profit}")
+    print(f"Average Change: ${round(avg_change,2)}")
+    print(f"Greatest Increase in Profits: {month_list[highest_month]} (${int(greatest_inc)})")
+    print(f"Greatest Decrease in Profits: {month_list[lowest_month]} (${int(greatest_dec)})")
+
+
+    #print output into txt file
+    sys.stdout=open(output_path,"w")
+    print("Financial Analysis")
+    print("----------------------")
+    print(f"Total Months: {total_month}")
+    print(f"Total: {total_profit}")
+    print(f"Average Change: ${round(avg_change,2)}")
+    print(f"Greatest Increase in Profits: {month_list[highest_month]} (${int(greatest_inc)})")
+    print(f"Greatest Decrease in Profits: {month_list[lowest_month]} (${int(greatest_dec)})")    
+    sys.stdout.close()
